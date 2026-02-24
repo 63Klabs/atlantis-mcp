9b70ee876af19977a93db1d46156200613fc5188
chadkluck <kl5@chadkluck.net>
2026-02-24 13:56:46 -0600

    updated settings.js for env

--- application-infrastructure/src/config/settings.js
+++ application-infrastructure/src/config/settings.js
@@ -1,10 +1,13 @@
 // const { tools: {DebugAndLog} } = require("@63klabs/cache-data");
 
+const ATLANTIS_GITHUB_USER_ORGS = 
+	process.env?.ATLANTIS_GITHUB_USER_ORGS ? process.env.ATLANTIS_GITHUB_USER_ORGS.split(',') : ['63klabs'];
+
 const settings =  {
 	"errorExpirationInSeconds": 300,
 	"routeExpirationInSeconds": 3600,
 	"externalRequestHeadroomInMs": 8000,
-	"githubUsers": process.env.ATLANTIS_GITHUB_USER_ORGS.split(','),
+	"githubUsers": ATLANTIS_GITHUB_USER_ORGS,
 }
 
 module.exports = settings;
