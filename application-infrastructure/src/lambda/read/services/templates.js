d5e98942a349f8dbba1713649e1a279f320b4ef9
chadkluck <kl5@chadkluck.net>
2026-03-14 18:05:59 -0500

    fully updated services/templates.js for error messaging and cachedata access

--- application-infrastructure/src/lambda/read/services/templates.js
+++ application-infrastructure/src/lambda/read/services/templates.js
@@ -88,6 +88,7 @@ async function list(options = {}) {
     const list = await Models.S3Templates.list(connection, opts);
     DebugAndLog.debug(`${logName}.fetchFunction: Fetched templates from S3`, {
       count: list.templates?.length || 0,
+      list
     });
 
     // >! We need to wrap the list in a response format suitable for CacheableDataAccess
